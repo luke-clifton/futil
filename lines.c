c4c9bd14fe9573a8173394513086a6a6573f5c3b
Luke Clifton <lukec@themk.net>
2019-10-01 08:51:47 +0800

    Allow lines to be called with arg

--- lines.c
+++ lines.c
@@ -5,15 +5,18 @@
 #include <string.h>
 #include <unistd.h>
 
-// Split stdin into an array of lines.
-int main(int argc, char *argv[])
+// With no arguments, split stdin into an array of lines.
+// Otherwise, exec the argument, and split the output of
+// that function into lines.
+
+void go(int fd)
 {
 	char buf[BUFSIZ];
 	ssize_t len;
 	char *pre = buf;
 	char *cur = buf;
 	char zero = 0;
-	while ((len = read(0, buf, sizeof(buf))))
+	while ((len = read(fd, buf, sizeof(buf))))
 	{
 		if (len == -1)
 		{
@@ -39,5 +42,35 @@ int main(int argc, char *argv[])
 			write(1,pre, buf + len - pre);
 		}
 	}
-	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		go(0);
+	}
+	else
+	{
+		int fds[2];
+		if (pipe(fds))
+		{
+			perror("lines.pipe");
+			exit(0);
+		}
+		switch (fork())
+		{
+			case -1:
+				perror("lines.fork");
+				exit(1);
+			case 0:
+				close(fds[0]);
+				dup2(fds[1],1);
+				execvp(argv[1], &argv[1]);
+			default:
+				close(fds[1]);
+				go(fds[0]);
+				wait(NULL);
+		}
+	}
 }
