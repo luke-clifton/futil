8e9e11d4b4ac8e7e8df1fe3fb17d3f5fbc9800bc
Luke Clifton <lukec@themk.net>
2019-10-04 18:54:53 +0800

    List returns a single result

--- length.c
+++ length.c
@@ -7,6 +7,6 @@ int main(int argc, char *argv[])
 	ctx c = (ctx){.in = stdin, .out = stdout};
 	for(i = 0; read_item(&c); i++);
 	char buf[40] = {0};
-	printf("%d%c", i, 0);
+	printf("%d", i);
 	return 0;
 }
