5392069c6f39c2f734bd03ec7b6a6f2bb3c40787
Luke Clifton <lukec@themk.net>
2019-10-04 15:54:27 +0800

    Fix array

--- array.c
+++ array.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <futil.h>
 
 // Construct an array out of a variable number of command line arguments.
 
 int main(int argc, char *argv[])
 {
-	while(*(++argv)) write(1, *argv, strlen(*argv) + 1);
+	ctx c = (ctx){.out = stdout};
+	while(*(++argv)) write_item(&c, *argv);
 }
--- zip_.c
+++ zip_.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <futil.h>
 
 // Version of zip that works with files (useful with process substitution!)
 //
@@ -16,22 +17,18 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 	char *linep = NULL;
-	size_t s;
 	char *linep2 = NULL;
-	size_t s2;
-	int stat;
+	ctx out = (ctx){.out = stdout};
 	FILE *h1 = fopen(argv[1], "r");
 	FILE *h2 = fopen(argv[2], "r");
-	int tail = 0;
-	while (  (getdelim(&linep, &s, 0, h1) > 0)
-	      && (getdelim(&linep2, &s2, 0, h2) > 0)
+	ctx c1 = (ctx){.in = h1};
+	ctx c2 = (ctx){.in = h2};
+	while (  (linep = read_item(&c1))
+	      && (linep2 = read_item(&c2))
 	      )
 	{
-		if (tail) putchar(0);
-		tail = 1;
-		fputs(linep, stdout);
-		putchar(0);
-		fputs(linep2, stdout);
+		write_item(&out, linep);
+		write_item(&out, linep2);
 	}
 	fclose(h1);
 	fclose(h2);
