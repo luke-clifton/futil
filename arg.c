ccf51b5ac3c07369daf46c7f0b49252bbfd6d237
Luke Clifton <lukec@themk.net>
2019-10-05 17:18:42 +0800

    Typo

--- arg.c
+++ arg.c
@@ -28,9 +28,15 @@ int main(int argc, char *argv[])
 			close(fds[1]);
 			dup2(fds[0], 0);
 			execvp(argv[2], &argv[2]);
+			perror("arg");
+			exit(1);
 		default:
 			close(fds[0]);
 			if (-1 == write(fds[1], argv[1], strlen(argv[1])))
+			{
+				perror("arg");
+				exit(1);
+			}
 			close(fds[1]);
 			if (wait(&stat) == -1)
 			{
