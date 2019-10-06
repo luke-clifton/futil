8ae9b4d267f9b0caa2b0f789333b1df706a73459
Luke Clifton <lukec@themk.net>
2019-10-07 07:57:51 +0800

    Rename a few things, delay negative

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CFLAGS = -Wall -pedantic -lskarnet -lfutil
+CFLAGS = -Wall -pedantic -lskarnet -lfutil -lm
 
 SRCS=$(wildcard *.c)
 LIBS_SRCS=$(wildcard lib/*.c)
--- delay.c
+++ delay.c
@@ -1,12 +1,43 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <math.h>
+#include <sys/wait.h>
 
 int main(int argc, char *argv[])
 {
-	unsigned int usecs = atoi(argv[1]);
-	usleep(usecs);
-	execvp(argv[2], &argv[2]);
-	perror("delay");
-	exit(1);
+	double secs = strtod(argv[1], NULL);
+	unsigned int usecs = round(abs(secs * 1000000));
+	
+	if (secs >= 0)
+	{
+		usleep(usecs);
+		if (argv[2])
+		{
+			execvp(argv[2], &argv[2]);
+			perror("delay");
+			exit(1);
+		}
+		exit(0);
+	}
+
+	int stat;
+	switch (fork())
+	{
+		case -1:
+			perror("delay");
+			exit(1);
+		case 0:
+			execvp(argv[2], &argv[2]);
+			perror("delay");
+			exit(1);
+		default:
+			wait(&stat);
+			if (WIFEXITED(stat))
+			{
+				usleep(usecs);
+				exit(WEXITSTATUS(stat));
+			}
+			exit(1);
+	}
 }
