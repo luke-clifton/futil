1bb6f3835cea8d78a989b4dd57d4419612001455
Luke Clifton <lukec@themk.net>
2019-10-07 09:12:46 +0800

    Added readList

--- readList.c
+++ readList.c
@@ -3,14 +3,15 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
-// Like cat, but without the options and will error out if the file
-// contains a \0 byte.
+// Like cat, but without the options, and will ensure that the output is
+// '\0' terminated.
 //
-// See also readList
+// See also readFile
 
 int main(int argc, char *argv[])
 {
 	char buf[BUFSIZ];
+	int terminated = 1;
 	while (*(++argv))
 	{
 		FILE *handle = fopen(*argv, "r");
@@ -22,21 +23,18 @@ int main(int argc, char *argv[])
 		int len;
 		while ((len = fread(buf, 1, sizeof(buf), handle)))
 		{
-			if (memchr(buf, 0, len))
-			{
-				fprintf(stderr, "readFile: %s contains a 0 byte. Terminating.", *argv);
-				exit(1);
-			}
 			if (len > fwrite(buf, 1, len, stdout))
 			{
 				perror("readFile");
 				exit(1);
 			}
+			terminated = !buf[len - 1];
 		}
 		if (ferror(handle))
 		{
 			perror("readFile.fread");
 			exit(1);
 		}
+		if (!terminated) fputc(0, stdout);
 	}
 }
