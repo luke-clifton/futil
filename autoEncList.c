6b299c78c3944c424d83fdf83f1ff4efa21d3538
Luke Clifton <lukec@themk.net>
2019-10-08 11:34:07 +0800

    Add >>= option to autoEncList

--- autoEncList.c
+++ autoEncList.c
@@ -6,21 +6,57 @@
 
 // Construct an array out of a variable number of command line arguments.
 
+char **command(char **argv)
+{
+	while (*argv)
+	{
+		if (!strcmp(*argv, ">>="))
+			return argv + 1;
+		argv++;
+	}
+	return NULL;
+}
+
 int main(int argc, char *argv[])
 {
+	char **cmd = command(argv);
+	int fds[2];
+	FILE *h;
+	if (cmd)
+	{
+		pipe(fds);
+		switch (fork())
+		{
+			case -1:
+				perror("autoEncList");
+				exit(1);
+			case 0:
+				close(fds[1]);
+				dup2(fds[0], 0);
+				execvp(*cmd, cmd);
+				perror("autoEncList");
+				exit(1);
+			default:
+				close(fds[0]);
+				h = fdopen(fds[1], "w");
+				break;
+		}
+	}
 	argv++;
-	while (*argv)
+	while (*argv && strcmp(*argv, ">>="))
 	{
 		if (*argv && **argv == ':')
 		{
-			printf("%c", 0);
+			fprintf(h, "%c", 0);
 			argv++;
 		}
-		while(*argv && (**argv != ':'))
+		
+		while(*argv && strcmp(*argv, ":") && strcmp(*argv, ">>="))
 		{
-			printf("%s\1\1", *argv++);
+			fprintf(h, "%s\1\1", *argv++);
 		}
 	}
-	printf("%c", 0);
+	fprintf(h, "%c", 0);
+	fclose(h);
+	wait(NULL);
 }
-
