eb4aad0b5fec7a3891c4ac5d8ba41642002c276d
Luke Clifton <lukec@themk.net>
2021-11-10 14:02:56 +0800

    tests

--- sp/test.sh
+++ sp/test.sh
@@ -18,3 +18,4 @@ expect '1\x002\x003\x00' pipeline list 1 2 3 '' flatmaps a : printf '%s' a
 expect '1\x002\x003\x00' pipeline list 1 2 3 '' format '%s\0'
 expect '12\x0034\x00' pipeline list 1 2 3 4 '' flatmaps a b : printf '%s' a b
 expect '-\0-\0' pipeline fix cons - '' head -c 4
+expect '1\n1\n2\n3\n5\n8\n13\n' unlines take 7 fix cons 1 cons 1 calc read peek + p reset
